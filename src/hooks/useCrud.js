5d8600dfbab58fe33a7655a50dd9c7836f84fcab
bismalemmanuel <111012723+bismalemmanuel@users.noreply.github.com>
2022-12-16 16:50:32 -0400

    Update useCrud.js

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -17,7 +17,7 @@ const useCrud = () => {
        
        
        const createNewUser =(data)=>{
-        const URL='http://users-crud.academlo.tech/users/'
+        const URL='https://users-crud.academlo.tech/users/'
         axios.post(URL,data)
         .then(()=> getAllUsers())
         .catch(err=>console.log(err)) 
@@ -25,14 +25,14 @@ const useCrud = () => {
        }
     
        const deleteUserById=(id) =>{
-        const URL =`http://users-crud.academlo.tech/users/${id}/`
+        const URL =`https://users-crud.academlo.tech/users/${id}/`
         axios.delete(URL)
         .then(res=>getAllUsers())
         .catch(err=>console.log(err))
       }
     
       const udpateUserById =(id, data)=>{
-        const URL =`http://users-crud.academlo.tech/users/${id}/`
+        const URL =`https://users-crud.academlo.tech/users/${id}/`
         axios.put(URL,data)
         .then(res=>getAllUsers())
         .catch(err=>console.log(err))
